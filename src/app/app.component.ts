caac49eaf2d932d5045f0204611b82b0bf3524c2
Robert97 <hakobyan.robert97@gmail.com>
2018-09-04 19:59:14 +0400

    Upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,18 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   registerForm: FormGroup;
 
+  active = {
+    firstName: false,
+    lastName: false,
+    email: false,
+    areaCode1: false,
+    areaCode2: false,
+    pass: false,
+    rePass: false,
+  };
+
+  checked = false;
+
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -17,17 +29,21 @@ export class AppComponent implements OnInit {
 
   createRegisterForm() {
     this.registerForm = this._formBuilder.group({
-      firstName: [null, Validators.required],
-      lastName: [null, Validators.required],
-      email: [null, Validators.required],
+      firstName: [null, [Validators.required, Validators.pattern(/^[A-Z][a-z]+$/)]],
+      lastName: [null, [Validators.required, Validators.pattern(/^[A-Z][a-z]+$/)]],
+      email: [null, [Validators.required, Validators.pattern(/^[a-zA-Z0-9_.-]+@+[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,4}$/)]],
       areaCode1: [null, Validators.required],
       areaCode2: [null, Validators.required],
-      pass: [null, Validators.required],
+      pass: [null, [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)]],
       rePass: [null, Validators.required]
     });
   }
 
   onSubmit() {
-    window.location.reload();
+    this.checked = true;
+
+    if (this.registerForm.valid && this.registerForm.value.pass === this.registerForm.value.rePass) {
+      window.location.reload();
+    }
   }
 }
