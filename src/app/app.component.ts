735ad018b0c35e27fb238e2a0f4fb83793620d7f
Robert97 <hakobyan.robert97@gmail.com>
2018-09-04 17:40:30 +0400

    Upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng6-test-task';
+export class AppComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this._formBuilder.group({
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      email: [null, Validators.required],
+      areaCode1: [null, Validators.required],
+      areaCode2: [null, Validators.required],
+      pass: [null, Validators.required],
+      rePass: [null, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    window.location.reload();
+  }
 }
